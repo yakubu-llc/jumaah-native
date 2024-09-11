76a23be7c73c468d797e7ec46b6fdd61603f0452
kareem717 <kareem.717@icloud.com>
2024-09-10 23:35:40 -0400

    feat: simple native auth

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,32 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, Text } from 'react-native'
+import { Session } from '@supabase/supabase-js'
+import { useState, useEffect } from 'react'
+import { supabase } from '~/utils/supabase'
+import { Auth } from '~/components/auth/Auth';
 import { ScreenContent } from '~/components/ScreenContent';
+import { LogoutButton } from '~/components/auth/LogoutButton';
 
 export default function Home() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     <>
       <Stack.Screen options={{ title: 'Tab One1' }} />
       <View style={styles.container}>
         <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
+        {session ? <Text>Logged in, {session.user.id}</Text> : <Auth />}
+        <LogoutButton />
       </View>
     </>
   );
---
+++ components/auth/Auth.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export function Auth() {
+  return (
+    <View style={styles.container}>
+      <Text>TODO: web auth</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+});
